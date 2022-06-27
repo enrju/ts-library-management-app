45fcf564bd870ffec315b2782843a124d2c2d19d
enrju <enrju@enrju.pl>
2022-06-27 19:23:20 +0200

    (back): add basic Express server

---
+++ backend/config/config.example.ts
@@ -0,0 +1,4 @@
+// export const config = {
+//     listenPort: 3001,
+//     listenHost: 'localhost',
+// }
--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,14 @@
-console.log("Hellołłł");
+import express from 'express';
+import 'express-async-errors';
+import path from "path";
+import {config} from "./config/config";
+
+const app = express();
+
+app.use(express.json());
+//temporary
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(config.listenPort, config.listenHost, () => {
+    console.log('Server is working...');
+});
