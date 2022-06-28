4a3dc8b86ca0bca9272ec176df81ccdd1f9581d6
enrju <enrju@enrju.pl>
2022-06-28 11:01:39 +0200

    (back): add DB connection

--- backend/index.ts
+++ backend/index.ts
@@ -2,12 +2,19 @@ import express from 'express';
 import 'express-async-errors';
 import path from "path";
 import {config} from "./config/config";
+import {pool} from "./utils/db";
 
 const app = express();
 
 app.use(express.json());
-//temporary
+//temporary front-end
 app.use(express.static(path.join(__dirname, 'public')));
+
+//test db-connection
+(async () => {
+    const [results] = await pool.execute("SELECT * FROM `users`");
+    console.log(results);
+})();
 
 app.listen(config.listenPort, config.listenHost, () => {
     console.log('Server is working...');
