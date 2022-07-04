4d9e422888703c126fa4c2758ec0ceb10a27a244
enrju <enrju@enrju.pl>
2022-07-04 09:52:04 +0200

    (db): modify DB example dump

--- db_example/db_tables_example.sql
+++ db_example/db_tables_example.sql
@@ -16,6 +16,33 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `book_authors`
+--
+
+DROP TABLE IF EXISTS `book_authors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book_authors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name_surname` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book_authors`
+--
+
+LOCK TABLES `book_authors` WRITE;
+/*!40000 ALTER TABLE `book_authors` DISABLE KEYS */;
+INSERT INTO `book_authors` VALUES (1,'author-1');
+INSERT INTO `book_authors` VALUES (2,'author-2');
+INSERT INTO `book_authors` VALUES (3,'author-3');
+INSERT INTO `book_authors` VALUES (4,'author-4');
+/*!40000 ALTER TABLE `book_authors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `book_states`
 --
 
@@ -42,6 +69,33 @@ INSERT INTO `book_states` VALUES (3,'rented');
 UNLOCK TABLES;
 
 --
+-- Table structure for table `book_titles`
+--
+
+DROP TABLE IF EXISTS `book_titles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book_titles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book_titles`
+--
+
+LOCK TABLES `book_titles` WRITE;
+/*!40000 ALTER TABLE `book_titles` DISABLE KEYS */;
+INSERT INTO `book_titles` VALUES (1,'title-1');
+INSERT INTO `book_titles` VALUES (2,'title-2');
+INSERT INTO `book_titles` VALUES (3,'title-3');
+INSERT INTO `book_titles` VALUES (4,'title-4');
+/*!40000 ALTER TABLE `book_titles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `books`
 --
 
@@ -50,17 +104,27 @@ DROP TABLE IF EXISTS `books`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `books` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `author` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `title` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `state` int(11) NOT NULL,
+  `author1_id` int(11) NOT NULL,
+  `author2_id` int(11) DEFAULT NULL,
+  `author3_id` int(11) DEFAULT NULL,
+  `title_id` int(11) NOT NULL,
+  `book_state_id` int(11) NOT NULL,
   `user_id` varchar(36) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `return_until` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
-  KEY `state` (`state`),
+  KEY `state` (`book_state_id`),
+  KEY `author1_id` (`author1_id`),
+  KEY `author2_id` (`author2_id`),
+  KEY `author3_id` (`author3_id`),
+  KEY `title_id` (`title_id`),
   CONSTRAINT `books_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `books_ibfk_2` FOREIGN KEY (`state`) REFERENCES `book_states` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  CONSTRAINT `books_ibfk_2` FOREIGN KEY (`book_state_id`) REFERENCES `book_states` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `books_ibfk_3` FOREIGN KEY (`author1_id`) REFERENCES `book_authors` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `books_ibfk_4` FOREIGN KEY (`author2_id`) REFERENCES `book_authors` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `books_ibfk_5` FOREIGN KEY (`author3_id`) REFERENCES `book_authors` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `books_ibfk_6` FOREIGN KEY (`title_id`) REFERENCES `book_titles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,14 +133,14 @@ CREATE TABLE `books` (
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (1,'autor-1','tytuł-1',1,NULL,NULL);
-INSERT INTO `books` VALUES (2,'autor-1','tytuł-1',1,NULL,NULL);
-INSERT INTO `books` VALUES (3,'autor-1','tytuł-1',1,NULL,NULL);
-INSERT INTO `books` VALUES (4,'autor-2','tytuł-2',1,NULL,NULL);
-INSERT INTO `books` VALUES (5,'autor-2','tytuł-2',1,NULL,NULL);
-INSERT INTO `books` VALUES (6,'autor-3','tytuł-3',1,NULL,NULL);
-INSERT INTO `books` VALUES (7,'autor-4','tytuł-4',1,NULL,NULL);
-INSERT INTO `books` VALUES (8,'autor-4','tytuł-4',1,NULL,NULL);
+INSERT INTO `books` VALUES (1,1,NULL,NULL,1,1,NULL,NULL);
+INSERT INTO `books` VALUES (2,1,NULL,NULL,1,1,NULL,NULL);
+INSERT INTO `books` VALUES (3,1,NULL,NULL,1,1,NULL,NULL);
+INSERT INTO `books` VALUES (4,2,NULL,NULL,2,1,NULL,NULL);
+INSERT INTO `books` VALUES (5,2,NULL,NULL,2,1,NULL,NULL);
+INSERT INTO `books` VALUES (6,3,NULL,NULL,3,1,NULL,NULL);
+INSERT INTO `books` VALUES (7,4,NULL,NULL,4,1,NULL,NULL);
+INSERT INTO `books` VALUES (8,4,NULL,NULL,4,1,NULL,NULL);
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -116,10 +180,11 @@ CREATE TABLE `users` (
   `id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
   `login` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` int(11) NOT NULL DEFAULT 1,
+  `user_role_id` int(11) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
-  KEY `role` (`role`),
-  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role`) REFERENCES `user_roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  UNIQUE KEY `login` (`login`),
+  KEY `role` (`user_role_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`user_role_id`) REFERENCES `user_roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -129,11 +194,11 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES ('abc','tester-1','abc',1);
-INSERT INTO `users` VALUES ('def','tester-2','def',1);
-INSERT INTO `users` VALUES ('ghi','tester-3','ghi',2);
-INSERT INTO `users` VALUES ('jkl','tester-4','jkl',1);
-INSERT INTO `users` VALUES ('mno','tester-5','mno',1);
+INSERT INTO `users` VALUES ('abc','tester-1','tester-1',1);
+INSERT INTO `users` VALUES ('def','tester-2','tester-2',1);
+INSERT INTO `users` VALUES ('ghi','tester-3','tester-3',2);
+INSERT INTO `users` VALUES ('jkl','tester-4','tester-4',1);
+INSERT INTO `users` VALUES ('mno','tester-5','tester-5',1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -146,4 +211,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-27 17:53:29
+-- Dump completed on 2022-07-04  9:48:32
