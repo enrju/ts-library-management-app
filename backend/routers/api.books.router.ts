2f9af0770d49a3ca4832f71545ade5f26984755e
enrju <enrju@enrju.pl>
2022-07-05 12:19:17 +0200

    (back): update router apiBooksRouter [GET] '/books/:page/:per_page', add [GET] '/books'

--- backend/routers/api.books.router.ts
+++ backend/routers/api.books.router.ts
@@ -1,7 +1,38 @@
 import express, {Request, Response} from "express";
+import {Cookies} from "../utils/cookies";
+import {createdSessions} from "../utils/sessions";
+import {BookRecord} from "../records/book.record";
 
 export const apiBooksRouter = express.Router();
 
+apiBooksRouter.get('/books', (req: Request, res: Response) => {
+    const session_id = Cookies.getSessionId(req);
+
+    let userRole = createdSessions.getRole(session_id);
+
+    if(userRole) {
+
+        (async () => {
+            const allBooks = await BookRecord.findAll();
+
+            res.json(allBooks);
+        })();
+
+    } else {
+        res.json({access: false});
+    }
+});
+
 apiBooksRouter.get('/books/:page/:per_page', (req: Request, res: Response) => {
-    res.send('lista pierwszych 5 książek (domyślnie)');
+    const session_id = Cookies.getSessionId(req);
+
+    let userRole = createdSessions.getRole(session_id);
+
+    if(userRole) {
+
+        res.redirect('/api/books');
+
+    } else {
+        res.json({access: false});
+    }
 });
