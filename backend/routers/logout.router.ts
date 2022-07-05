44a71c43ed9cb63ec3fcecb2125698fa20e12b63
enrju <enrju@enrju.pl>
2022-07-05 12:13:15 +0200

    (back): update router logoutRouter [POST] '/logout'

--- backend/routers/logout.router.ts
+++ backend/routers/logout.router.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response} from "express";
-import {session} from "../utils/sessions";
+import {createdSessions} from "../utils/sessions";
 import {Cookies} from "../utils/cookies";
 
 export const logoutRouter = express.Router();
@@ -7,7 +7,7 @@ export const logoutRouter = express.Router();
 logoutRouter.post('/', (req: Request, res: Response) => {
     const {session_id} = req.cookies;
 
-    session.remove(session_id);
+    createdSessions.remove(session_id);
     Cookies.remove(res);
     
     res.send("Wylogowano");
