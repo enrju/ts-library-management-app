36d86128bc5439dc0ef6822a100b1b24f4202d1e
enrju <enrju@enrju.pl>
2022-08-13 22:12:12 +0200

    (back): install and configure TypeORM, prepare DB (XAMPP)

---
+++ backend/config/orm-config.example.ts
@@ -0,0 +1,14 @@
+// import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+//
+// export const ormConfig: TypeOrmModuleOptions = {
+//     type: "mysql",
+//     host: "localhost",
+//     port: 3306,
+//     username: "your-db-user",
+//     password: "your-db-password",
+//     database: "your-data-base",
+//     entities: ["dist/**/**.entity{.ts,.js}"],
+//     bigNumberStrings: false,
+//     logging: true,
+//     synchronize: true,
+// };
