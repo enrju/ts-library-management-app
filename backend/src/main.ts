007372dcb59058bc35911c829947c0393822048f
enrju <enrju@enrju.pl>
2022-08-15 16:49:56 +0200

    (back): install and configure CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appConfig } from "../config/app-config";
+import {NestExpressApplication} from "@nestjs/platform-express";
+const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  (app as NestExpressApplication).use(cors({
+    origin: appConfig.corsOrigin,
+  }));
+
   await app.listen(appConfig.listenPort, () => {
     console.log('Server is working ...');
   });
