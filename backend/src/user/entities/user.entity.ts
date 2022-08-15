c20b794c0ee1ed6bc181df2eb374eaebf5b7a02e
enrju <enrju@enrju.pl>
2022-08-15 14:27:45 +0200

    (back): add UserEntity in /user/entities/user.entity.ts

---
+++ backend/src/user/entities/user.entity.ts
@@ -0,0 +1,41 @@
+import {
+    BaseEntity,
+    Column,
+    Entity,
+    PrimaryGeneratedColumn
+} from "typeorm";
+
+@Entity()
+export class UserEntity extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({
+        length: 50,
+    })
+    name: string;
+
+    @Column({
+        length: 50,
+    })
+    surname: string;
+
+    @Column({
+        length: 255,
+    })
+    email: string;
+
+    @Column()
+    passwordHash: string;
+
+    @Column({
+        default: null,
+        nullable: true,
+    })
+    currentTokenId: string | null;
+
+    @Column({
+        default: 'user',
+    })
+    role: 'admin' | 'user';
+}
