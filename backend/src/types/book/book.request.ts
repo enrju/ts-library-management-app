4be8c34f29069d7363ebd8581de1f5c677e6d71e
enrju <enrju@enrju.pl>
2022-08-26 09:47:33 +0200

    (back): add interface UpdateBookRequest in /types/book/book.request.ts

--- backend/src/types/book/book.request.ts
+++ backend/src/types/book/book.request.ts
@@ -1,9 +1,15 @@
 export interface BookAuthor {
+    id?: number;
     name: string;
     surname: string;
 }
 
 export interface CreateBookRequest {
     title: string;
+    author: BookAuthor[];
+}
+
+export interface UpdateBookRequest {
+    title: {id: number, title: string};
     author: BookAuthor[];
 }
