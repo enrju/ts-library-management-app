c557ea57a4e3386b91953cf947435bcf5488c208
enrju <enrju@enrju.pl>
2022-08-31 17:23:48 +0200

    (front): update App component - add main structure to choose component to display

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import './App.scss';
+import {getObjectOfCookies} from "../../utils/get-object-of-cookies";
+import {UserRole} from 'types';
 
 export const App = () => {
-  return (
-      <>
-        <h1>Test frontu</h1>
-      </>
-  );
+    const cookies = getObjectOfCookies();
+    let whatDisplay = <h1>Coś poszło nie tak</h1>;
+
+    if(cookies.logged) {
+        switch(cookies.role) {
+            case UserRole.User:
+                whatDisplay = <h1>User Panel</h1>;
+                break;
+            case UserRole.Admin:
+                whatDisplay = <h1>Admin Panel</h1>;
+                break;
+        }
+    } else {
+        if(cookies.isRegisterForm === 'false'
+            || !cookies.isRegisterForm) {
+            whatDisplay = <h1>Login Form</h1>;
+        } else if(cookies.isRegisterForm === 'true') {
+            whatDisplay = <h1>Register Form</h1>;
+        }
+    }
+
+    return (
+        <>
+            {whatDisplay}
+        </>
+    )
 }
