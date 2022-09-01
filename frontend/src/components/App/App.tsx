542df2947a89129a292d624ae05cfb51449b696b
enrju <enrju@enrju.pl>
2022-09-01 21:39:49 +0200

    (front): add login-form

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 import {getObjectOfCookies} from "../../utils/get-object-of-cookies";
 import {UserRole} from 'types';
+import {LoginForm} from "../Form/Login-form";
 
 export const App = () => {
+    const [msgError, setMsgError] = useState<string | null>(null);
+    const [showRegisterForm, setShowRegisterForm] = useState<boolean>(false);
+
     const cookies = getObjectOfCookies();
-    let whatDisplay = <h1>Coś poszło nie tak</h1>;
+
+    let componentToDisplay = <h1>Coś poszło nie tak</h1>;
 
     if(cookies.logged) {
         switch(cookies.role) {
             case UserRole.User:
-                whatDisplay = <h1>User Panel</h1>;
+                componentToDisplay = <h1>User Panel</h1>;
                 break;
             case UserRole.Admin:
-                whatDisplay = <h1>Admin Panel</h1>;
+                componentToDisplay = <h1>Admin Panel</h1>;
                 break;
         }
     } else {
         if(cookies.isRegisterForm === 'false'
             || !cookies.isRegisterForm) {
-            whatDisplay = <h1>Login Form</h1>;
+            componentToDisplay =
+                <LoginForm
+                    msgError={msgError}
+                    setMsgError={setMsgError}
+                    setShowRegisterForm={setShowRegisterForm}
+                />;
         } else if(cookies.isRegisterForm === 'true') {
-            whatDisplay = <h1>Register Form</h1>;
+            componentToDisplay = <h1>Register Form</h1>;
         }
     }
 
     return (
         <>
-            {whatDisplay}
+            {componentToDisplay}
         </>
     )
 }
